d26e8a5bdaa3a7898337d54a676cdc74dafe304c
Brad Newman <newmanbradm@gmail.com>
2018-05-19 21:31:23 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,9 +1,11 @@
 def dictionary
   dictionary = {
-    hello: hi, 
-    to: 2,
-    two: 2,
-    too: 2,
-    for: 4,
+    "hello" => hi, 
+    "to" => 2,
+    "two" => 2,
+    "too" => 2,
+    "for" => 4,
+    "four" => 4,
+    "be" => 
     
   }
