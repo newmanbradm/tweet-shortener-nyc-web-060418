8ff2506170d9bdfd4acf11eafab23e79732c50e1
Brad Newman <newmanbradm@gmail.com>
2018-05-19 21:51:10 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -38,7 +38,7 @@ def selective_tweet_shortener(tweet)
 end
 
 def shortened_tweet_truncator(tweet)
-  if word_substituter(tweet) > 140
+  if word_substituter(tweet).length > 140
     word_substituter(tweet)[0..136] << "..."
   else
     tweet
