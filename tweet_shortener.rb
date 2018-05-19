e661f2120ba64abec26b3f9ebfc6d61ded3f0f37
Brad Newman <newmanbradm@gmail.com>
2018-05-19 21:39:39 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,5 +17,8 @@ def word_substituter(string)
   string.split.collect do |word|
     if dictionary.keys.include?(word.downcase)
       word = dictionary[word.downcase]
+    else
+      word
     end
-    
+  end.join(" ")
+end
